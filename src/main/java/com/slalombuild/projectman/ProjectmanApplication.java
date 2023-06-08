c9e4fa430bdc05e894df4ab3662515ea97606dd8
Jose Chavez <jose.chavez.slalom.com@kellerhome.com>
2023-06-08 10:53:54 -0700

    Added spotless

--- src/main/java/com/slalombuild/projectman/ProjectmanApplication.java
+++ src/main/java/com/slalombuild/projectman/ProjectmanApplication.java
@@ -1,3 +1,4 @@
+/* (C) 2023 */
 package com.slalombuild.projectman;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectmanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectmanApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectmanApplication.class, args);
+  }
 }
--- src/test/java/com/slalombuild/projectman/ProjectmanApplicationTests.java
+++ src/test/java/com/slalombuild/projectman/ProjectmanApplicationTests.java
@@ -1,3 +1,4 @@
+/* (C) 2023 */
 package com.slalombuild.projectman;
 
 import org.junit.jupiter.api.Test;
@@ -6,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectmanApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
