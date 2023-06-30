f5c8bd88e08ee5d28d1ca134209bb7dbdbbd3aef
Jose Chavez <jose.chavez@slalom.com>
2023-06-30 11:55:40 -0700

    Changing test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 -- Test data for demo
-INSERT INTO PROJECT (ID, MARKET_NAME, NAME) VALUES (1, 'Seattle', 'TMobile Customer Enhancement');
+INSERT INTO PROJECT (ID, MARKET_NAME, NAME) VALUES (1, 'Seattle', 'Telecom Customer Enhancement');
