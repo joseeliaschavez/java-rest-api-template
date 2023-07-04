2c74c7f36ebbc9856abd594c01378df96722f35b
Jose Chavez <jose.chavez@slalom.com>
2023-07-04 16:03:30 -0700

    Added ProjectService unit tests

--- src/test/java/com/slalombuild/projectman/domain/service/ProjectServiceTests.java
+++ src/test/java/com/slalombuild/projectman/domain/service/ProjectServiceTests.java
@@ -1,4 +1,34 @@
 /* (C) 2023 */
 package com.slalombuild.projectman.domain.service;
 
-public class ProjectServiceTests {}
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+import com.slalombuild.projectman.domain.entity.Project;
+import com.slalombuild.projectman.domain.repository.ProjectRepository;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectServiceTests {
+  @Mock private ProjectRepository projectRepository;
+  @InjectMocks private ProjectService service;
+
+  @Test
+  public void givenProjects_whenGetAllProjects_thenReturnList() {
+    // Arrange
+    var expectedResults = Instancio.ofList(Project.class).size(5).create();
+    when(projectRepository.findAll()).thenReturn(expectedResults);
+
+    // Act
+    var actualResult = service.getAllProjects();
+
+    // Assert
+    assertThat(actualResult).isNotNull();
+    verify(projectRepository, atLeastOnce()).findAll();
+  }
+}
