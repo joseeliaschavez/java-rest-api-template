6730d6df0f078ce9e7bd9244e9fbc6ee197d8375
Jose Chavez <jose.chavez@slalom.com>
2023-07-26 13:54:55 -0700

    Added test coverage for TmdbMovieRepository

---
+++ src/test/java/com/slalombuild/movieman/utils/FileUtils.java
@@ -0,0 +1,14 @@
+/* (C) 2023 */
+package com.slalombuild.movieman.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class FileUtils {
+  public static String asString(String pathToFile) throws IOException {
+    Path path = Paths.get(pathToFile);
+    return Files.readAllLines(path).get(0);
+  }
+}
