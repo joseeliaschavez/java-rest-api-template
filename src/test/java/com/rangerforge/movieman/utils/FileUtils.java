36345eb064d964dbd6646c8defc8b04da660f51b
Jose Chavez <jose.chavez@dialexa.com>
2024-07-26 20:24:45 -0700

    Cleaned up PMD rule violations for unit tests

--- src/test/java/com/rangerforge/movieman/utils/FileUtils.java
+++ src/test/java/com/rangerforge/movieman/utils/FileUtils.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class FileUtils {
+  private FileUtils() {}
+
   public static String asString(String pathToFile) throws IOException {
     Path path = Paths.get(pathToFile);
     return Files.readAllLines(path).get(0);
